7adf67d71b1e6287173f8d2a884b42056bf2bcd3
Nathan Oliveira <nathanoliveira3@gmail.com>
2021-05-12 14:01:47 -0300

    alteração do código.

--- ProjetoFinal/src/br/com/serratec/principal/MainImposto.java
+++ ProjetoFinal/src/br/com/serratec/principal/MainImposto.java
@@ -59,7 +59,7 @@ public class MainImposto {
 					String nomeFuncionario = scan.nextLine();
 					System.out.println("Informe o CPF do funcionário");
 					String cpfFuncionario = scan.nextLine();
-					System.out.println("Informe a dadata de nascimento do funcionário");
+					System.out.println("Informe a data de nascimento do funcionário");
 					String data = scan.nextLine();
 					LocalDate dataNascimento = LocalDate.parse(data, df);
 					System.out.println("Informe o salário do funcionário");
@@ -70,17 +70,22 @@ public class MainImposto {
 						if (!funcionarios.contains(func)) {
 							funcionarios.add(func);
 							System.out.println("Funcionário adicionado com sucesso!");
+						} else {
+							System.out.println("Funcionário já cadastrado.");
 						}
 					} catch (CPFException e) {
 						func.setCpf("00000000003");
 						System.out.println("O CPF do funcionario " + func.getNome()
-								+ " possui mais de 11 digitos. foi atribuido o valor padrão de " + func.getCpf()
+								+ "está em formato inválido. Foi atribuido o valor padrão de " + func.getCpf()
 								+ " Faça a alteração no menu.");
 						funcionarios.add(func);
 					}
 				} catch (DateTimeParseException e) {
 					System.out.println("Data inválida. O formato correto é dd/MM/yyy.");
+				} catch (InputMismatchException e) {
+					System.out.println("Salário inválido.");
 				}
+				scan.nextLine();
 				break;
 			case 3:
 				System.out.println("Informe o CPF do funcionário");
@@ -90,18 +95,23 @@ public class MainImposto {
 			case 4:
 				System.out.println("Informe o CPF do funcionário a remover");
 				String funcExcluir = scan.nextLine();
-				funcionarios.removeIf(f -> f.getCpf().equalsIgnoreCase(funcExcluir));
-				System.out.println("Funcionário excluido com sucesso");
+				if (funcionarios.removeIf(r -> r.getCpf().equalsIgnoreCase(funcExcluir))) {
+					System.out.println("Funcionário excluido com sucesso");
+				} else {
+					System.out.println("Funcionário não cadastrado.");
+				}
 				break;
 			case 5:
-				System.out.println("Informe o CPF do funcionário");
-				String depExcluir = scan.nextLine();
+				System.out.println("Informe o CPF do dependente a excluir");
+				String excluir = scan.nextLine();
 				for (Funcionario f : funcionarios) {
-					if (f.getCpf().equalsIgnoreCase(depExcluir)) {
-						System.out.println("Informe o CPF do dependente a excluir");
-						String excluir = scan.nextLine();
-						f.getDependente().removeIf(d -> d.getCpf().equalsIgnoreCase(excluir));
+					if (f.getDependente().removeIf(r -> r.getCpf().equalsIgnoreCase(excluir))) {
+						System.out.println("Dependente excluído com sucesso!");
+					} else {
+						System.out.println("Dependente não cadastrado.");
 					}
+
+					break;
 				}
 				break;
 			case 6:
@@ -123,6 +133,8 @@ public class MainImposto {
 				}
 			case 10:
 				break;
+			default:
+				System.out.println("Opção inválida.");
 			}
 
 		} while (opcao != 10);
@@ -139,7 +151,7 @@ public class MainImposto {
 					int opcao = 0;
 
 					do {
-						System.out.println("1 - Alterar nome:");
+						System.out.println("\n1 - Alterar nome:");
 						System.out.println("2 - Alterar CPF:");
 						System.out.println("3 - Alterar data de nascimento:");
 						System.out.println("4 - Alterar parentesco:");
@@ -156,9 +168,9 @@ public class MainImposto {
 							System.out.println("Nome alterado!");
 							break;
 						case 2:
-							System.out.println("Informe o novo CPF: ");
-							String novoCpf = scan.nextLine();
 							try {
+								System.out.println("Informe o novo CPF: ");
+								String novoCpf = scan.nextLine();
 								d.verificarCPF(novoCpf);
 								d.setCpf(novoCpf);
 								System.out.println("CPF alterado!");
@@ -168,10 +180,10 @@ public class MainImposto {
 							}
 							break;
 						case 3:
-							System.out.println("Informe a data de nascimento: ");
-							String nascimento = scan.nextLine();
-							DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 							try {
+								System.out.println("Informe a data de nascimento: ");
+								String nascimento = scan.nextLine();
+								DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 								LocalDate dataNascimento = LocalDate.parse(nascimento, df);
 								d.setDataNascimento(dataNascimento);
 								System.out.println("Data de nascimento alterada!");
@@ -180,20 +192,31 @@ public class MainImposto {
 							}
 							break;
 						case 4:
-							System.out.println("Informe o parentesco: ");
-							String tipo = scan.nextLine();
-							TipoDependente parentesco = TipoDependente.valueOf(tipo);
-							d.setParentesco(parentesco);
-							System.out.println("Parentesco alterado!");
+							System.out
+									.println("Informe o parentesco do dependente: 1 - FILHO; 2 - SOBRINHO; 3 - OUTRO");
+							int tipo = scan.nextInt();
+							scan.nextLine();
+							TipoDependente tipoDependente = null;
+							if (tipo == 1) {
+								tipoDependente = TipoDependente.FILHO;
+							} else if (tipo == 2) {
+								tipoDependente = TipoDependente.SOBRINHO;
+							} else if (tipo == 3) {
+								tipoDependente = TipoDependente.OUTRO;
+							} else {
+								System.out.println("Parentesco inválido.");
+								break;
+							}
+							d.setParentesco(tipoDependente);
 							break;
 						case 5:
 							break;
-
+						default:
+							System.out.println("Opção inválida");
 						}
 					} while (opcao != 5);
 				}
 			}
-
 		}
 	}
 
@@ -219,9 +242,9 @@ public class MainImposto {
 						System.out.println("Nome alterado!");
 						break;
 					case 2:
-						System.out.println("Informe o novo CPF: ");
-						String novoCpf = scan.nextLine();
 						try {
+							System.out.println("Informe o novo CPF: ");
+							String novoCpf = scan.nextLine();
 							f.verificarCPF(novoCpf);
 							f.setCpf(novoCpf);
 							System.out.println("CPF alterado!");
@@ -230,10 +253,10 @@ public class MainImposto {
 						}
 						break;
 					case 3:
-						System.out.println("Informe a data de nascimento: ");
-						String nascimento = scan.nextLine();
-						DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 						try {
+							System.out.println("Informe a data de nascimento: ");
+							String nascimento = scan.nextLine();
+							DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 							LocalDate dataNascimento = LocalDate.parse(nascimento, df);
 							f.setDataNascimento(dataNascimento);
 							System.out.println("Data de nascimento alterada!");
@@ -242,64 +265,76 @@ public class MainImposto {
 						}
 						break;
 					case 4:
-						System.out.println("Informe o novo salário: ");
 						try {
+							System.out.println("Informe o novo salário: ");
 							double salario = scan.nextDouble();
 							f.setSalarioBruto(salario);
 							System.out.println("Salário alterado!");
 						} catch (InputMismatchException e) {
 							System.out.println("Salário inválido.");
 						}
+						scan.nextLine();
 						break;
 					case 5:
 						break;
+					default:
+						System.out.println("Opção inválida.");
 
 					}
 				} while (opcao != 5);
 			}
 		}
-
 	}
 
 	private static void incluirDependente(List<Funcionario> funcionarios, Scanner scan, String cpfFuncDep) {
 		for (Funcionario f : funcionarios) {
 			if (f.getCpf().equalsIgnoreCase(cpfFuncDep)) {
-				System.out.println("Informe o nome do dependente");
-				String nomeDependente = scan.nextLine();
-				System.out.println("Informe o CPF do dependente");
-				String cpfDependente = scan.nextLine();
-				System.out.println("Informe a dadata de nascimento do dependente");
-				String data = scan.nextLine();
-				DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-				LocalDate dataNascimento = LocalDate.parse(data, df);
-				System.out.println("Informe o parentesco do dependente: 1 - FILHO; 2 - SOBRINHO; 3 - OUTRO");
-				int tipo = scan.nextInt();
-				scan.nextLine();
-				TipoDependente tipoDependente = null;
-				if (tipo == 1) {
-					tipoDependente = TipoDependente.FILHO;
-				} else if (tipo == 2) {
-					tipoDependente = TipoDependente.SOBRINHO;
-				} else if (tipo == 3) {
-					tipoDependente = TipoDependente.OUTRO;
-				} else {
-					System.out.println("Parentesco inválido.");
-					break;
-				}
-				Dependente dep = new Dependente(nomeDependente, cpfDependente, dataNascimento, tipoDependente);
+				Dependente dep = null;
 				try {
+					System.out.println("Informe o nome do dependente");
+					String nomeDependente = scan.nextLine();
+					System.out.println("Informe o CPF do dependente");
+					String cpfDependente = scan.nextLine();
+					System.out.println("Informe a dadata de nascimento do dependente");
+					String data = scan.nextLine();
+					DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+					LocalDate dataNascimento = LocalDate.parse(data, df);
+
+					System.out.println("Informe o parentesco do dependente: 1 - FILHO; 2 - SOBRINHO; 3 - OUTRO");
+					int tipo = scan.nextInt();
+					scan.nextLine();
+					TipoDependente tipoDependente = null;
+					if (tipo == 1) {
+						tipoDependente = TipoDependente.FILHO;
+					} else if (tipo == 2) {
+						tipoDependente = TipoDependente.SOBRINHO;
+					} else if (tipo == 3) {
+						tipoDependente = TipoDependente.OUTRO;
+					} else {
+						System.out.println("Parentesco inválido.");
+						break;
+					}
+					dep = new Dependente(nomeDependente, cpfDependente, dataNascimento, tipoDependente);
+
 					dep.verificarIdade();
 					dep.verificarCPF(cpfDependente);
-					f.adicionarDependente(dep);
-					System.out.println("Dependente adicionado com sucesso!");
+					if (!f.getDependente().contains(dep)) {
+						f.getDependente().add(dep);
+						System.out.println("Dependente adicionado com sucesso!");
+					} else {
+						System.out.println("Dependente já cadastrado.");
+					}					
 				} catch (DependenteException e) {
 					System.out.println(dep.getNome() + " não pode ser um dependente, pois tem mais de 18 anos.");
 				} catch (CPFException e) {
 					dep.setCpf("00000000005");
 					f.adicionarDependente(dep);
-					System.out.println("O CPF do dependente " + dep.getNome() + " possui mais de 11 digitos. foi atribuido o valor padrão de "+ dep.getCpf()+ " Faça a alteração no menu.");
+					System.out.println("O CPF do dependente " + dep.getNome()
+							+ " está em formato inválido. Foi atribuido o valor padrão de " + dep.getCpf()
+							+ " Faça a alteração no menu.");
+				} catch (DateTimeParseException e) {
+					System.out.println("Data inválida.");
 				}
-				
 			}
 		}
 	}
--- ProjetoFinal/src/br/com/serratec/servicos/LeituraArquivo.java
+++ ProjetoFinal/src/br/com/serratec/servicos/LeituraArquivo.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,68 +17,91 @@ import br.com.serratec.enums.TipoDependente;
 import br.com.serratec.exception.CPFException;
 import br.com.serratec.exception.DependenteException;
 
-public class LeituraArquivo {	
-	public void lerArquivo( List<Funcionario> funcionarios, Scanner scan, String arquivoNome ) {
+public class LeituraArquivo {
+	public void lerArquivo(List<Funcionario> funcionarios, Scanner scan, String arquivoNome) {
 		try {
 			File arquivo = new File(arquivoNome);
-			scan = new Scanner(new InputStreamReader(new FileInputStream(arquivo),"UTF-8"));
-			
+			scan = new Scanner(new InputStreamReader(new FileInputStream(arquivo), "UTF-8"));
+
 			Funcionario funcionario = null;
 			while (scan.hasNext()) {
-				String linha = scan.nextLine(); 
+				String linha = scan.nextLine();
 				if (!linha.isEmpty()) {
-					if (funcionario != null) {
+					if (funcionario != null) {	
+						Dependente dependente = null;
+						
 						String[] dadosDependente = linha.split(";");
 						String nomeDependente = dadosDependente[0];
+						try {
 						String cpfDependente = dadosDependente[1];
 						LocalDate dataAniversario = LocalDate.parse(dadosDependente[2]);
 						TipoDependente parentesco = TipoDependente.valueOf(dadosDependente[3]);
-						Dependente dependente = new Dependente(nomeDependente, cpfDependente, dataAniversario, parentesco);						
-						try {
+						dependente = new Dependente(nomeDependente, cpfDependente, dataAniversario,
+								parentesco);
 						dependente.verificarIdade();
 						dependente.verificarCPF(cpfDependente);
-						}catch(DependenteException e) {
-							System.out.println(dependente.getNome() + " não foi adicionado pois possui idade superior a 18 anos.");
-							continue;							
-						}catch(CPFException e) {
+						}catch(IllegalArgumentException e) {
+							System.out.println(nomeDependente + " não foi adicionado pois possui um parentesco inválido.");
+						}catch(DateTimeParseException e) {
+							System.out.println(nomeDependente + " não foi adicionado, pois a data de nascimento é inválida.");					
+							//continue;
+						}catch (DependenteException e) {
+							System.out.println(
+							dependente.getNome() + " não foi adicionado pois possui idade superior a 18 anos.");
+							//continue;
+						} catch (CPFException e) {
 							dependente.setCpf("000000000001");
-							System.out.println("O CPF do dependente " + dependente.getNome() + " possui mais de 11 digitos. foi atribuido o valor padrão de "+ dependente.getCpf()+ " Faça a alteração no menu.");
-						}						
-						if(funcionario.getDependente().contains(dependente)) {
+							System.out.println("O CPF do dependente " + dependente.getNome()
+									+ " possui mais de 11 digitos. foi atribuido o valor padrão de "
+									+ dependente.getCpf() + " Faça a alteração no menu.");
+						}
+						if (funcionario.getDependente().contains(dependente)) {
 							System.out.println("O dependente " + dependente.getNome() + " já foi cadastrado.");
-						}else {
+						} else if(!funcionario.getDependente().contains(dependente) && dependente != null){
 							funcionario.adicionarDependente(dependente);
-						}						
+						}
 						continue;
-					} else {					
-						 String[] dadosFuncionario = linha.split(";");
-						 String nomeFuncionario = dadosFuncionario[0];					
-						 String cpfFuncionario = dadosFuncionario[1];						 
-						 LocalDate dataAniversario = LocalDate.parse(dadosFuncionario[2]);
-						 double salarioBruto = Double.parseDouble(dadosFuncionario[3]);
-						 funcionario = new Funcionario(nomeFuncionario,cpfFuncionario, dataAniversario, salarioBruto); 
-						 try {
-						 funcionario.verificarCPF(cpfFuncionario);
-						 }catch(CPFException e){
-							 funcionario.setCpf("00000000002");
-							 System.out.println("O CPF do funcionario " + funcionario.getNome() + " possui mais de 11 digitos. foi atribuido o valor padrão de "+ funcionario.getCpf()+ " Faça a alteração no menu.");
-						 }
-					 }
+					} else {
+						
+						String[] dadosFuncionario = linha.split(";");
+						String nomeFuncionario = dadosFuncionario[0];
+						String cpfFuncionario = dadosFuncionario[1];
+						try {
+						LocalDate dataAniversario = LocalDate.parse(dadosFuncionario[2]);
+						double salarioBruto = Double.parseDouble(dadosFuncionario[3]);
+						funcionario = new Funcionario(nomeFuncionario, cpfFuncionario, dataAniversario, salarioBruto);
+						
+							funcionario.verificarCPF(cpfFuncionario);
+						} catch (CPFException e) {
+							funcionario.setCpf("00000000002");
+							System.out.println("O CPF do funcionario " + funcionario.getNome()
+									+ " possui mais de 11 digitos. foi atribuido o valor padrão de "
+									+ funcionario.getCpf() + " Faça a alteração no menu.");
+						}catch(DateTimeParseException e) {
+							System.out.println(nomeFuncionario + " não foi cadastrado pois a data de nascimento informada é inválida.");
+						}catch (InputMismatchException e) {
+							System.out.println(nomeFuncionario + " não foi cadastrado pois o salário informado é inválido.");
+						}catch(NumberFormatException e) {
+							System.out.println(nomeFuncionario + " não foi cadastrado pois o salário informado é inválido.");
+						}
+					}
 				} else {
-					if(funcionario != null) {
-						if(funcionarios.contains(funcionario)) {
-							System.out.println("Funcionário " + funcionario.getNome() + " já cadastrado.");							
-						}else {
+					if (funcionario != null) {
+						if (funcionarios.contains(funcionario)) {
+							System.out.println("Funcionário " + funcionario.getNome() + " já cadastrado.");
+						} else {
 							funcionarios.add(funcionario);
 							funcionario = null;
-						}						
-				}	}
-			}	
+						}
+					}
+				}
+			}
 			funcionarios.add(funcionario);
-		} catch (FileNotFoundException e) {			
-			System.out.println("Arquivo não encontrado, verifique o caminho informado.");;
-		} catch (UnsupportedEncodingException e) {			
+		} catch (FileNotFoundException e) {
+			System.out.println("Arquivo não encontrado, verifique o caminho informado.");
+			;
+		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
-		}	
+		}
 	}
 }
